152aaab35e9e37f1f12760148e7483db781ceb9b
abdolrhman-mo <abdelrhamnlearn@gmail.com>
2024-09-11 11:22:28 +0300

    edited next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+const url = new URL(process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000')
+
 const nextConfig = {
     images: {
       remotePatterns: [
         {
-            protocol: new URL(process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000').protocol.replace(':', ''),
-            hostname: new URL(process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000').hostname,
-            port: new URL(process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000').port || '',
+            protocol: url.protocol.replace(':', ''),
+            hostname: url.hostname,
+            port: url.port || '',
             pathname: '/media/products/**',
         },
     ],
